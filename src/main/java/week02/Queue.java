831479569128ea802ff95f227c7690a69abab734
Timo <developer.timo@gmail.com>
2022-10-25 16:58:12 +0900

    refactor: 패키지명 변경

--- src/main/java/week02/Queue.java
+++ src/main/java/week02/Queue.java
@@ -1,4 +1,4 @@
-package ch02;
+package week02;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/week02/QueueTest.java
+++ src/test/java/week02/QueueTest.java
@@ -1,4 +1,4 @@
-package ch02;
+package week02;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
