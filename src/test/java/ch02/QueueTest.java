c0c06a9d44f692a34d73ed10ae5638a2c31933ed
Timo <developer.timo@gmail.com>
2022-10-25 16:05:07 +0900

    test: 큐에 값을 삽입하는 테스트 케이스 작성

--- src/test/java/ch02/QueueTest.java
+++ src/test/java/ch02/QueueTest.java
@@ -18,4 +18,17 @@ class QueueTest {
         //then
         Assertions.assertEquals(queue.getCapacity(), size);
     }
+
+    @DisplayName("큐에 값을 삽입한다")
+    void enqueue() {
+        //given
+        Queue queue = Queue.create(3);
+        int item = 10;
+
+        //when
+        queue.enqueue(item);
+
+        //then
+        Assertions.assertEquals(1, queue.size);
+    }
 }
